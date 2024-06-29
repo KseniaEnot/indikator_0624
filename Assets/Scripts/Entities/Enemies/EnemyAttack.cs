30d76c62a61e145589947152a5e7e015d3ca4536
v.todorov <$7920A1212#>
2024-06-29 18:49:50 +0300

    EnemyAttack

--- Assets/Scripts/Entities/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Entities/Enemies/EnemyAttack.cs
@@ -1,12 +1,41 @@
+using System;
+using Cysharp.Threading.Tasks;
+using Entities.HP;
 using UnityEngine;
 
 namespace Entities.Enemies
 {
     public class EnemyAttack : MonoBehaviour
     {
+        [SerializeField] private float _radius;
+        [SerializeField] private float _timeAttack;
+        [SerializeField] private float _damage;
+        
+        private Transform _target;
+        private IHealth _targetHealth;
+        private bool _isDestroy;
+
         public void SetTarget(Transform target)
         {
-            
+            _target = target;
+            _targetHealth = target.GetComponent<IHealth>();
+            Attack().Forget();
+        }
+
+        private void OnDestroy()
+        {
+            _isDestroy = true;
+        }
+
+        private async UniTask Attack()
+        {
+            while (_target)
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(_timeAttack));
+                
+                if (!_isDestroy && (transform.position - _target.position).magnitude < _radius) 
+                    _targetHealth.DoDamage(_damage);
+            }
         }
     }
 }
